a696a1d0fa0d0ce1af642645e8928229b5e80d67
Nainlpz <nain.lopez.2003@gmail.com>
2025-03-20 13:41:11 +0100

    feat(Item): Crear interfaz Item

---
+++ src/main/java/edu/estatuas/stockx/Item.java
@@ -0,0 +1,14 @@
+package edu.estatuas.stockx;
+
+interface Item {
+
+    int getBid();
+    void setBid(int bid);
+
+    int getAsk();
+    void setAsk(int ask);
+
+    int getSale();
+    void setSale(int sale);
+
+}

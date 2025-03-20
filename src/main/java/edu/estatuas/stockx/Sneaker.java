88b3afc4dc21b86ba99a9a1f276aa1d291b7e65a
Nainlpz <nain.lopez.2003@gmail.com>
2025-03-20 13:41:28 +0100

    feat(Sneaker): Crear clase Sneaker

---
+++ src/main/java/edu/estatuas/stockx/Sneaker.java
@@ -0,0 +1,59 @@
+package edu.estatuas.stockx;
+
+public class Sneaker implements Item {
+
+    private String style = "";
+    private String name = "";
+    private int sale = 0;
+    private int ask = 0;
+    private int bid = 0;
+
+    public Sneaker(String style, String name) {
+        this.style = style;
+        this.name = name;
+    }
+
+    public String getStyle() {
+        return this.style;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    @Override
+    public int getBid() {
+        return this.bid;
+    };
+
+    @Override
+    public void setBid(int bid) {
+        this.bid = bid;
+    };
+
+    @Override
+    public int getAsk() {
+        return this.ask;
+    };
+
+    @Override
+    public void setAsk(int ask) {
+        this.ask = ask;
+    };
+
+    @Override
+    public int getSale() {
+        return this.sale;
+    };
+
+    @Override
+    public void setSale(int sale) {
+        this.sale = sale;
+    };
+
+    @Override
+    public String toString() {
+        return "\t" + this.name + "\n\t\t" + this.style;
+    }
+
+}

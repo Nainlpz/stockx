bba88972d1f9246bdbc398febb39785839c910d4
Nainlpz <nain.lopez.2003@gmail.com>
2025-03-21 12:30:39 +0100

    feat(Bid): Crear clase bid

---
+++ src/main/java/edu/estatuas/stockx/Bid.java
@@ -0,0 +1,33 @@
+package edu.estatuas.stockx;
+
+public class Bid implements Offer{
+
+    private String size = "";
+    private Integer bid = 0;
+
+    public Bid(String size, Integer bid) {
+        this.size = size;
+        this.bid = bid;
+    }
+
+    @Override
+    public String size() {
+        return this.size;
+    }
+
+    @Override
+    public int value() {
+        return this.bid;
+    }
+
+    @Override
+    public int compareTo(Offer bid) {
+        return this.bid.compareTo(bid.value());
+    }
+
+    @Override
+    public String toString() {
+        return "\t\t" + this.size() + "\t" + this.value() +"\n";
+    }
+
+}

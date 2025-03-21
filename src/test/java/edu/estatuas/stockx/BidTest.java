238aaee63c019e2c7766236f6ddaa73fcde5f619
Nainlpz <nain.lopez.2003@gmail.com>
2025-03-21 12:31:04 +0100

    test(BidTest): Pasar casos test Bid

---
+++ src/test/java/edu/estatuas/stockx/BidTest.java
@@ -0,0 +1,39 @@
+package edu.estatuas.stockx;
+
+import org.junit.Before;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class BidTest {
+
+    private Bid bid;
+
+    @Before
+    public void setUp() {
+        bid = new Bid("9.5", 300);
+    }
+
+    @Test
+    public void testSize() {
+        assertEquals("9.5", bid.size());
+    }
+
+    @Test
+    public void testValue() {
+        assertEquals(300, bid.value());
+    }
+
+    @Test
+    public void testCompareTo() {
+        Bid higherBid = new Bid("9.5", 350);
+        Bid lowerBid = new Bid("9.5", 250);
+        assertTrue(bid.compareTo(higherBid) < 0);
+        assertTrue(bid.compareTo(lowerBid) > 0);
+        assertTrue(bid.compareTo(bid) == 0);
+    }
+
+    @Test
+    public void testToString() {
+        assertEquals("\t\t9.5\t300\n", bid.toString());
+    }
+}

ba38a16dd53129c793f2e70847e3af0c08b86047
Nainlpz <nain.lopez.2003@gmail.com>
2025-03-24 14:57:16 +0100

    feat(Sale): Crear clase Sale

---
+++ src/main/java/edu/estatuas/stockx/Item/Sale.java
@@ -0,0 +1,33 @@
+package edu.estatuas.stockx.Item;
+
+public class Sale implements Offer{
+
+    private String size = "";
+    private Integer sale = 0;
+
+    public Sale(String size, Integer sale) {
+        this.size = size;
+        this.sale = sale;
+    }
+
+    @Override
+    public String size() {
+        return this.size;
+    }
+
+    @Override
+    public int value() {
+        return this.sale.intValue();
+    }
+
+    @Override
+    public int compareTo(Offer sale) {
+        return this.sale.compareTo(sale.value());
+    }
+
+    @Override
+    public String toString() {
+        return "\t\t" + this.size() + "\t" + this.value() +"\n";
+    }
+
+}
